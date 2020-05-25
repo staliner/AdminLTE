c2683fb87bc50369753b279e179e7b5d0d1e60a9
DL6ER <dl6er@dl6er.de>
2020-05-26 01:16:21 +0200

    Add per-browser boxed layout setting
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/footer.js
+++ scripts/pi-hole/js/footer.js
@@ -130,8 +130,24 @@ function applyCheckboxRadioStyle() {
   if (chkboxStyle === null) {
     chkboxStyle = "material-blue";
   }
+
   $("input[type='radio'],input[type='checkbox']").parent().removeClass();
-  $("input[type='radio'],input[type='checkbox']").parent().addClass("icheck-" + chkboxStyle);
+  $("input[type='radio'],input[type='checkbox']")
+    .parent()
+    .addClass("icheck-" + chkboxStyle);
+}
+
+function applyBoxedLayout() {
+  var boxedlayout = localStorage.getItem("boxedlayout");
+  if (boxedlayout === null) {
+    boxedlayout = true;
+  }
+
+  if (boxedlayout === true) {
+    $("body").addClass("layout-boxed");
+  } else {
+    $("body").removeClass("layout-boxed");
+  }
 }
 
 $(document).ready(function () {
@@ -146,7 +162,9 @@ $(document).ready(function () {
     $("#cookieInfo").show();
   }
 
+  // Apply per-browser styling settings
   applyCheckboxRadioStyle();
+  applyBoxedLayout();
 
   // Run check immediately after page loading ...
   checkMessages();
